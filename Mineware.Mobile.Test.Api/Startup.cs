755fcafa0bfd05a596bcb9b4bbf4a79ac4cb937e
Justin Karouz <justin@mineware.co.za>
2021-06-08 20:30:47 +0200

    fixed swagger

--- Mineware.Mobile.Test.Api/Startup.cs
+++ Mineware.Mobile.Test.Api/Startup.cs
@@ -46,7 +46,7 @@ namespace Mineware.Mobile.Test.Api
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
+			if (env.IsDevelopment() || env.IsProduction())
 			{
 				app.UseDeveloperExceptionPage();
 				app.UseSwagger();
